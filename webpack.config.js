018e6207d4b5478c40b8d8cc5bf286e53e476393
anyars-encarta <anyarsencarta@gmail.com>
2023-07-18 15:50:28 +0000

    Correct linter files setup

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Webpack App',
+            title: 'To Do List',
             filename: 'index.html',
             template: 'src/template.html',
         }),
